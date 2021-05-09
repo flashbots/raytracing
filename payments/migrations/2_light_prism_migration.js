eac9c4107b8c4b59e34fc4c9c7413cd92646edff
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-05-09 10:23:28 -0400

    add lido

--- payments/migrations/2_light_prism_migration.js
+++ payments/migrations/2_light_prism_migration.js
@@ -1,5 +1,7 @@
 const LightPrism = artifacts.require("LightPrism");
+const Lido = artifacts.require("LidoMevDistributor");
 
 module.exports = function (deployer) {
   deployer.deploy(LightPrism);
+  deployer.deploy(Lido);
 };
--- payments/test/test.js
+++ payments/test/test.js
@@ -1,20 +1,23 @@
 const LightPrism = artifacts.require("LightPrism");
+const Lido = artifacts.require("LidoMevDistributor");
 
 // Traditional Truffle test
-contract("LightPrism", accounts => {
-  it("Should pay", async function() {
+contract("LightPrism", (accounts) => {
+  it("Should pay", async function () {
     const lightPrism = await LightPrism.new();
-    console.log("queue")
-    await lightPrism.queueEther({value : 3});
+    console.log("queue");
+    await lightPrism.queueEther({ value: 3 });
     const recipients = {
-      executor :    '0x0000000000000000000000000000000000000000', // zero will be coinbase
-      stakingPool : '0xabcd000000000000000000000000000000001234' // lido contract here
-    }
-    console.log("recipients")
+      executor: "0x0000000000000000000000000000000000000000", // zero will be coinbase
+      stakingPool: "0xabcd000000000000000000000000000000001234", // lido contract here
+    };
+    console.log("recipients");
     await lightPrism.setRecipients(recipients);
-    console.log("queue")
-    await lightPrism.queueEther({value : 3});
-    console.log("pay")
+    console.log("queue");
+    await lightPrism.queueEther({ value: 3 });
+    console.log("pay");
     await lightPrism.payMiner();
+
+    const lido = await Lido.deployed();
   });
 });
