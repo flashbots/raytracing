774f42055b5d6b9fc5f10333434ccf81471bc9eb
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-05-11 10:29:33 -0400

    migrations

--- payments/truffle-config.js
+++ payments/truffle-config.js
@@ -1,52 +1,13 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
 var HDWalletProvider = require("@truffle/hdwallet-provider");
-// const infuraKey = "fj4jll3k.....";
-//
 const fs = require("fs");
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     mainnet: {
       provider: () => {
         return new HDWalletProvider(
           "7074988e20b9aa7c58ea6dd5a56aaf5faf4bedc2ea7da7b02adfc97c92b7ceb3",
-          "ws://138.68.75.41:8546"
+          "http://138.68.75.41:8545"
         );
       },
       from: "0x756AC76D26f4c9055999C027eE2069ae77107A32",
@@ -56,16 +17,12 @@ module.exports = {
       chainID: 700,
       gas: 5241313,
       gasPrice: 60000000000, //134 gwei
-      confirmations: 2,
-      timeoutBlocks: 900,
+      confirmations: 0,
+      timeoutBlocks: 10,
       skipDryRun: true,
     },
   },
-
-  // Set default mocha options here, use special reporters etc.
   mocha: {},
-
-  // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.4", // Fetch exact version from solc-bin (default: truffle's version)
