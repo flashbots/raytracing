ad608f586ed6886e92d3c3ce47884765f6ddc0b8
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-05-11 10:31:34 -0400

    add constructor param lido mev deploy

--- payments/migrations/1_migration.js
+++ payments/migrations/1_migration.js
@@ -3,5 +3,9 @@ const Lido = artifacts.require("LidoMevDistributor");
 
 module.exports = function (deployer) {
   deployer.deploy(LightPrism);
-  deployer.deploy(Lido);
+  deployer.deploy(
+    Lido,
+    `0x5d6d0199912b58220ac15661427af6bf53926385`,
+    `${1e18}`
+  );
 };
