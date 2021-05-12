4325a4cfa5f0cbb9ff2cb03967c51cd18579dc2d
tkstanczak <tkstanczak_github@demerzel.co>
2021-05-12 14:04:37 +0100

    pass

--- payments/migrations/1_migration.js
+++ payments/migrations/1_migration.js
@@ -1,24 +1,25 @@
 const LightPrism = artifacts.require("LightPrism");
 const LidoMevDistributor = artifacts.require("LidoMevDistributor");
-const NodeOperatorsRegistry = artifacts.require("NodeOperatorsRegistry");
-const DepositContractMock = artifacts.require("DepositContractMock");
-const Lido = artifacts.require("Lido");
+// const NodeOperatorsRegistry = artifacts.require("NodeOperatorsRegistry");
+// const DepositContractMock = artifacts.require("DepositContractMock");
+// const Lido = artifacts.require("Lido");
 
 module.exports = function (deployer) {
-  var operatorsAddress = deployer.deploy(NodeOperatorsRegistry);
-  var depositAddress = deployer.deploy(DepositContractMock);
-  console.log(address);
-  var lidoAddress = deployer.deploy(Lido,
-    depositAddress,
-    `0x0000000000000000000000000000000000000001`,
-    operatorsAddress,
-    `0x0000000000000000000000000000000000000002`,
-    `0x0000000000000000000000000000000000000003`,
-    );
-  deployer.deploy(
-    LidoMevDistributor,
-    lidoAddress,
-    `${4e18}`
-  );
+  // var operatorsAddress = deployer.deploy(NodeOperatorsRegistry);
+  // var depositAddress = deployer.deploy(DepositContractMock);
+  // var lidoAddress = deployer.deploy(Lido,
+  //   depositAddress,
+  //   `0x0000000000000000000000000000000000000001`,
+  //   operatorsAddress,
+  //   `0x0000000000000000000000000000000000000002`,
+  //   `0x0000000000000000000000000000000000000003`,
+  //   );
+  // var lidoAddress = '0x5304e3c2b42BEaA8f3e37585bFed1274D1055E47'
+  // var address = deployer.deploy(
+  //   LidoMevDistributor,
+  //   lidoAddress,
+  //   `${1e18}`
+  // );
+  // console.log(address);
   deployer.deploy(LightPrism);
 };
--- payments/test/test.js
+++ payments/test/test.js
@@ -5,11 +5,13 @@ const Lido = artifacts.require("LidoMevDistributor");
 contract("LightPrism", (accounts) => {
   it("Should pay", async function () {
     const lightPrism = await LightPrism.deployed();
-    const lido = await Lido.deployed();
+    // const lido = await Lido.deployed();
+    const lido = await Lido.at("0x5304e3c2b42BEaA8f3e37585bFed1274D1055E47");
+    // console.log(lido)
     console.log("queue");
-    await lightPrism.queueEther({ value: 3 });
-    const executor = "0x0000000000000000000000000000000000000000";
-    const stakingPool = "0x5f3519e5cbb1d4ba25efea577f61e5ced54a6cea";
+    await lightPrism.queueEther({ value: 100 });
+    const executor = "0x1230000000000000000000000000000000000321";
+    const stakingPool = "0x5304e3c2b42BEaA8f3e37585bFed1274D1055E47";
     // const recipients = {
     //   executor: "0x0000000000000000000000000000000000000000", // zero will be coinbase
     //   stakingPool: "0xabcd000000000000000000000000000000001234", // lido contract here
@@ -17,11 +19,12 @@ contract("LightPrism", (accounts) => {
     console.log("recipients");
     await lightPrism.setRecipients(executor, stakingPool);
     console.log("queue");
-    await lightPrism.queueEther({ value: 3 });
+    await lightPrism.queueEther({ value: 100 });
     console.log("pay");
     await lightPrism.payMiner();
     console.log("paid");
     await lido.distribureMev();
     console.log("MEV distributed");
+    await lido.explodeToSeeEvents();
   });
 });
