ba470d9fc4f5b0eb8102e6bf15fbb05d618858bc
tkstanczak <tkstanczak_github@demerzel.co>
2021-05-12 12:48:02 +0100

    minor fixes migr

--- payments/migrations/1_migration.js
+++ payments/migrations/1_migration.js
@@ -8,7 +8,6 @@ module.exports = function (deployer) {
   var operatorsAddress = deployer.deploy(NodeOperatorsRegistry);
   var depositAddress = deployer.deploy(DepositContractMock);
   console.log(address);
-  var lightPrismAddress = deployer.deploy(LightPrism);
   var lidoAddress = deployer.deploy(Lido,
     depositAddress,
     `0x0000000000000000000000000000000000000001`,
@@ -17,8 +16,9 @@ module.exports = function (deployer) {
     `0x0000000000000000000000000000000000000003`,
     );
   deployer.deploy(
+    LidoMevDistributor,
     lidoAddress,
-    `0x5d6d0199912b58220ac15661427af6bf53926385`,
     `${4e18}`
   );
+  deployer.deploy(LightPrism);
 };
